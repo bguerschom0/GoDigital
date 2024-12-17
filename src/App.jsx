1becb8ad2634955e362a6cc8deb4aa965d4d3152
bguerschom0 <bguerschom0@gmail.com>
2024-12-17 06:43:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ import UpdateRequest from './pages/stakeholder/UpdateRequest'
 import { AdminLayout } from './components/layout'
 import StakeholderReport from './pages/reports/StakeholderReport'
 
+import NewBackgroundCheck from './pages/background/NewBackgroundCheck'
+import PendingBackgroundChecks from './pages/background/PendingBackgroundChecks'
+import UpdateBackgroundCheck from './pages/background/UpdateBackgroundCheck'
+import ExpiredDocuments from './pages/background/ExpiredDocuments'
+import AllBackgroundChecks from './pages/background/AllBackgroundChecks'
+import BackgroundCheckReport from './pages/reports/BackgroundCheckReport'
+
 function App() {
   return (
     <Router>
@@ -26,6 +33,13 @@ function App() {
             <Route path="stakeholder/pending" element={<PendingRequests />} />
             <Route path="stakeholder/update" element={<UpdateRequest />} />
             <Route path="reports/stakeholder" element={<StakeholderReport />} />
+                      {/* Background Check Routes */}
+            <Route path="background/new" element={<NewBackgroundCheck />} />
+            <Route path="background/pending" element={<PendingBackgroundChecks />} />
+            <Route path="background/update" element={<UpdateBackgroundCheck />} />
+            <Route path="background/expired" element={<ExpiredDocuments />} />
+            <Route path="background/all" element={<AllBackgroundChecks />} />
+            <Route path="background/report" element={<BackgroundCheckReport />} />
           </Route>
 
           <Route path="/" element={<Navigate to="/admin/dashboard" replace />} />
