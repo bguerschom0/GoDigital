991059a267b5841a3ed6800ab47960ac9ca07817
bguerschom0 <bguerschom0@gmail.com>
2024-12-17 19:58:22 +0200

    Update index.js

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,6 @@
 // src/components/layout/index.js
+export { default as AdminLayout } from './AdminLayout'
+export { default as UserLayout } from './UserLayout'
 export { default as Header } from './Header'
 export { default as Footer } from './Footer'
 export { default as Sidebar } from './Sidebar'
-export { default as AdminLayout } from './AdminLayout'
