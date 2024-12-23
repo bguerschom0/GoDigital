bc544f3c5eab74583b33299d3f3fab2251f84d95
bguerschom0 <bguerschom0@gmail.com>
2024-12-23 13:58:09 +0200

    Update AdminRoutes.jsx

--- src/components/routes/AdminRoutes.jsx
+++ src/components/routes/AdminRoutes.jsx
@@ -7,25 +7,6 @@ import Dashboard from '@/pages/admin/Dashboard'
 import Users from '@/pages/admin/Users'
 import PagePermissions from '@/pages/admin/PagePermissions'
 
-// Stakeholder Pages
-import NewRequest from '@/pages/stakeholder/NewRequest'
-import PendingRequests from '@/pages/stakeholder/PendingRequests'
-import UpdateRequest from '@/pages/stakeholder/UpdateRequest'
-import DeleteRequest from '@/pages/stakeholder/DeleteRequest'
-import AllRequests from '@/pages/stakeholder/AllRequests'
-
-// Background Check Pages
-import NewBackgroundCheck from '@/pages/background/NewBackgroundCheck'
-import PendingBackgroundChecks from '@/pages/background/PendingBackgroundChecks'
-import UpdateBackgroundCheck from '@/pages/background/UpdateBackgroundCheck'
-import ExpiredDocuments from '@/pages/background/ExpiredDocuments'
-import AllBackgroundChecks from '@/pages/background/AllBackgroundChecks'
-import InternshipOverview from '@/pages/background/InternshipOverview'
-
-// Report Pages
-import StakeholderReport from '@/pages/reports/StakeholderReport'
-import BackgroundCheckReport from '@/pages/reports/BackgroundCheckReport'
-
 const AdminRoutes = () => (
   <Routes>
     <Route path="/" element={<AdminLayout />}>
@@ -36,31 +17,6 @@ const AdminRoutes = () => (
       {/* Admin-only Pages */}
       <Route path="users" element={<Users />} />
       <Route path="permissions" element={<PagePermissions />} />
-
-      {/* Stakeholder Routes */}
-      <Route path="stakeholder">
-        <Route path="new" element={<NewRequest />} />
-        <Route path="pending" element={<PendingRequests />} />
-        <Route path="update" element={<UpdateRequest />} />
-        <Route path="delete" element={<DeleteRequest />} />
-        <Route path="all" element={<AllRequests />} />
-      </Route>
-
-      {/* Background Check Routes */}
-      <Route path="background">
-        <Route path="new" element={<NewBackgroundCheck />} />
-        <Route path="pending" element={<PendingBackgroundChecks />} />
-        <Route path="update" element={<UpdateBackgroundCheck />} />
-        <Route path="expired" element={<ExpiredDocuments />} />
-        <Route path="all" element={<AllBackgroundChecks />} />
-        <Route path="internship" element={<InternshipOverview />} />
-      </Route>
-
-      {/* Report Routes */}
-      <Route path="reports">
-        <Route path="stakeholder" element={<StakeholderReport />} />
-        <Route path="background" element={<BackgroundCheckReport />} />
-      </Route>
 
       {/* Catch all */}
       <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
