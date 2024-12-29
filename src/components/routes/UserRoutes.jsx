fe757becb071a84c9a534f1b4ee660f56bb0743c
bguerschom0 <bguerschom0@gmail.com>
2024-12-29 23:19:02 +0200

    Update UserRoutes.jsx

--- src/components/routes/UserRoutes.jsx
+++ src/components/routes/UserRoutes.jsx
@@ -77,10 +77,10 @@ const UserRoutes = () => (
       </Route>
 
       {/* Security Services */}
-      <Route path="security_services">
-        <Route path="security_service_request" element={<SecurityServiceRequest />} />
-        <Route path="task_page" element={<TasksPage />} />
-      </Route>
+<Route path="security_services">
+  <Route path="security_service_request" element={<SecurityServiceRequest />} />
+  <Route path="task_page" element={<TasksPage />} />
+</Route>
 
       {/* Catch all */}
       <Route path="*" element={<Navigate to="/user/dashboard" replace />} />
